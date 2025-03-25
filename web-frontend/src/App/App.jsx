eee8aac76080390c105eb54c32949c748e42d3e5
alexcrist <alexecrist@gmail.com>
2025-03-25 15:39:18 -0700

    Add header / footer

--- web-frontend/src/App/App.jsx
+++ web-frontend/src/App/App.jsx
@@ -1,7 +1,38 @@
 import styles from "./App.module.css";
 
 const App = () => {
-    return <div className={styles.container}>hello werld</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <div className={styles.headerContent}>
+                    <div className={styles.headerImageContainer}>
+                        <img
+                            className={styles.headerImage1}
+                            src="/bridge2aivoice.png"
+                            alt="Bridge2AI Voice logo"
+                        />
+                        <img
+                            className={styles.headerImage2}
+                            src="/bridge2aivoice.png"
+                            alt="Bridge2AI Voice logo"
+                        />
+                    </div>
+                    <div className={styles.headerSeparator} />
+                    <h1 className={styles.headerText}>
+                        Map Datasets Dashboard
+                    </h1>
+                    <img
+                        className={styles.waveImage}
+                        src="/wave.png"
+                        alt="Wave design"
+                    />
+                </div>
+                <div className={styles.headerGradientLine}></div>
+            </div>
+            <div className={styles.content}></div>
+            <div className={styles.footer}></div>
+        </div>
+    );
 };
 
 export default App;
